2f06f6040029def29125cf4cbd7096aef089da8f
Pravin Mane <manepravin11122@gmail.com>
2024-07-06 14:49:06 +0530

    Added hero section

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { ModeToggleComponent } from '../../../features/mode-toggle/mode-toggle.component';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ModeToggleComponent, NavigationComponent],
+  imports: [ModeToggleComponent, NavigationComponent, NgOptimizedImage],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
--- src/app/core/components/portfolio/portfolio.component.ts
+++ src/app/core/components/portfolio/portfolio.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { HeroComponent } from '../../../features/hero/hero.component';
+import { AboutComponent } from '../../../features/about/about.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, HeroComponent, AboutComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
