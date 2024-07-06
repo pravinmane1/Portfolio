b1f2cf378b02e1c508843899b59debb651c79dbf
Pravin Mane <manepravin11122@gmail.com>
2024-07-06 14:43:43 +0530

    Added hero section

---
+++ src/app/features/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss',
+})
+export class HeroComponent {}
