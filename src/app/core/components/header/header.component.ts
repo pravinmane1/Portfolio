0e554e9f82bbfd2c94a4f6a9e8a6804fd18eab77
Pravin Mane <manepravin11122@gmail.com>
2024-07-09 01:54:22 +0530

    Added header auto hide and mobile design handled

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,18 +1,33 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { ModeToggleComponent } from '../../../features/mode-toggle/mode-toggle.component';
 import { NavigationComponent } from '../navigation/navigation.component';
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ModeToggleComponent, NavigationComponent, NgOptimizedImage],
+  imports: [
+    ModeToggleComponent,
+    NavigationComponent,
+    NgOptimizedImage,
+    CommonModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private currentScrollY = 0;
+  public isHeaderVisible = signal<boolean>(true);
   @HostListener('window:scroll', ['$event'])
   scrollHandler(event: any) {
-    console.log(event.target.scrollTop,'Scroll Event');
+    const newScrollY = event?.currentTarget?.scrollY;
+    console.log(newScrollY, 'Scroll Event');
+
+    if (newScrollY && Math.abs(newScrollY - this.currentScrollY) > 10) {
+      const scrolledTowardUp = newScrollY < this.currentScrollY;
+      const headerCrossed = newScrollY > 100;
+      this.isHeaderVisible.set(!headerCrossed || scrolledTowardUp);
+      this.currentScrollY = event.currentTarget.scrollY;
+    }
   }
 }
--- src/app/features/mode-toggle/mode-toggle.component.ts
+++ src/app/features/mode-toggle/mode-toggle.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, Signal } from '@angular/core';
 import { ModeToggleService } from './mode-toggle.service';
+import { Mode } from './mode-toggle.model';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mode-toggle',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mode-toggle.component.html',
   styleUrl: './mode-toggle.component.scss',
 })
-export class ModeToggleComponent {
-  constructor(private modeToggleService: ModeToggleService) {}
+export class ModeToggleComponent implements OnInit {
+  private mode!: Signal<Mode>;
+  public isLightMode!: Signal<boolean>;
+  constructor(private modeToggleService: ModeToggleService) {
+    this.mode = toSignal(this.modeToggleService.modeChanged$) as Signal<Mode>;
+    this.isLightMode = computed(() => this.mode() === Mode.LIGHT);
+  }
+  ngOnInit(): void {}
   toggle() {
     this.modeToggleService.toggleMode();
   }
