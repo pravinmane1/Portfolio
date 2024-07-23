a51da036d882d937ce2cf58a8d3fb9876f7a3854
Pravin Mane <manepravin11122@gmail.com>
2024-07-23 08:47:59 +0530

    Added work experience component

--- src/app/core/components/portfolio/portfolio.component.ts
+++ src/app/core/components/portfolio/portfolio.component.ts
@@ -3,11 +3,18 @@ import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { HeroComponent } from '../../../features/hero/hero.component';
 import { AboutComponent } from '../../../features/about/about.component';
+import { WorkExperienceComponent } from '../../../features/work-experience/work-experience.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, HeroComponent, AboutComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutComponent,
+    WorkExperienceComponent,
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
