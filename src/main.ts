090c6ab845ec818d4fd6a4c27d493c71666c221a
Pravin Mane <39382220+pravinmane1@users.noreply.github.com>
2025-03-25 20:33:51 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+fetch('/config.json')
+  .then(response => response.json())
+  .then(config => {
+    (window as any).config = config;
+    bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+  }).catch((err)=>{
+   bootstrapApplication(AppComponent, appConfig)
+    .catch((err) => console.error(err));
+  });
